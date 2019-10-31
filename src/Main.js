3653566a77a6b1be9d3cc21875709655c1016b65
unknown <ericshi98@yahoo.com>
2019-10-31 09:46:07 -0400

    Set up react frontend. Backend express servers data to front end react through routes.

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+class Main extends Component{
+   render(){
+      return(
+         <div>
+            <h1>Hello World</h1>
+         </div>
+      );
+   }
+}
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Main.js';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
