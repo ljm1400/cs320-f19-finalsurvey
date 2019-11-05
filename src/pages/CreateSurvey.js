0d32c895399bfdfb6b3b51d9b066a499efe812c4
aryan singh <aryansingh@umass.edu>
2019-11-05 12:56:02 -0500

    frontend

--- src/pages/CreateSurvey.js
+++ src/pages/CreateSurvey.js
@@ -1,6 +1,9 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import QuestionList from './QuestionList';
-import '../css/style.css';
+import uuidv4 from 'uuid/v4';
+
+
+const LOCAL_STORAGE_KEY = 'ultimate.questions'
 
 export default function MainContent(){
 
@@ -9,41 +12,57 @@ export default function MainContent(){
     // the first argument is the state, and the second is the function we will call on that state
     // so the first will be the questions and the second will be the function which we can call to do something with the questions
 
+    useEffect(() => {
+        const storedQuestions = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        if (storedQuestions) setQuestions(storedQuestions)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(questions))
+    }, [questions])
+
     const questionNameRef = useRef()
     function handleAddQuestion(e){
         const name = questionNameRef.current.value
         if(name === '') return
         setQuestions(prevQuestions => {
-            return [...prevQuestions, {id:1, name:name, complete:false}]
+            return [...prevQuestions, {id:uuidv4(), name:name, complete:false}]
         })
+    }
+    function toggleQuestion(id) {
+        const newQuestions = [...questions]
+        const question = newQuestions.find(question => question.id === id)
+        question.complete = !question.complete
+        setQuestions(newQuestions)
+    }
+
+    function handleClearQuestions() {
+        const newQuestions = questions.filter(question => !question.complete)
+        setQuestions(newQuestions)
     }
 
     return(
         <>
-            <div className="header">
-                <h2>Create a Survey</h2>
-            </div>
-
             <div className='createSurvey'>
-                <label style={{fontSize:12}}>Question
+                <label style={{fontSize:17}}>Question
                     <input ref={questionNameRef} type="text" style={{margin:10}}/>
                 </label>
-                <label style={{fontSize:12}}>Multiple choice
+                <label style={{fontSize:17}}>Multiple choice
                     <input type="radio" style={{margin:10}} />
                 </label>
-                <label style={{fontSize:12}}>T/F
+                <label style={{fontSize:17}}>True/False
                     <input type="radio" style={{margin:10}} />
                 </label>
-                <label style={{fontSize:12}}>Text
+                <label style={{fontSize:17}}>Text
                     <input type="radio" style={{margin:10}} />
                 </label>
-                <label style={{fontSize:12}}>Slider
+                <label style={{fontSize:17}}>Slidersssss
                     <input type="radio" style={{margin:10}} />
                 </label>
 
                 <button onClick={handleAddQuestion}>Add Question</button>
-                <button>Remove Question</button>
-                <QuestionList questions={questions}/>
+                <button onClick={handleClearQuestions}>Remove Selected Question</button>
+                <QuestionList questions={questions} toggleQuestion={toggleQuestion}/>
             </div>
         </>
     )
