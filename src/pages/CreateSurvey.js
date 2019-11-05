00ff450ecf2c751d28650c33e9c17413f77fe0a6
aryan singh <aryansingh@umass.edu>
2019-11-05 14:16:45 -0500

    changes

--- src/pages/CreateSurvey.js
+++ src/pages/CreateSurvey.js
@@ -8,17 +8,13 @@ const LOCAL_STORAGE_KEY = 'ultimate.questions'
 export default function CreateSurvey(){
 
     const [questions,setQuestions] = useState([{id:1, name:'question', complete:false}])
+    const [count, setCount] = useState(0);
     // const [todos,setTodos] = useState(['question 1','question 2']) -> this was the earlier version
     // the first argument is the state, and the second is the function we will call on that state
     // so the first will be the questions and the second will be the function which we can call to do something with the questions
 
-    // this.state = {
-    //     size: ''
-    // };
     function handleChange(event) {
-        this.setState({
-          size: event.target.value
-        });
+        setCount(count + 1)
     }
     useEffect(() => {
         const storedQuestions = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -48,9 +44,6 @@ export default function CreateSurvey(){
         const newQuestions = questions.filter(question => !question.complete)
         setQuestions(newQuestions)
     }
-    // this.state = {
-    //     size: ''
-    // };
 
 
     return(
--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Question({ question,toggleQuestion }) {
   
   function handleQuestionClick() {
     toggleQuestion(question.id)
   }
-  const a = 0;
-  function Question(a){
+
+  var [a, setCount] = useState(1);
+  // var a = 1;
+  function Question(){
+    console.log(a);
     if(a === 1){
+      setCount(a+1)
       return <label>
       True <input style={{margin:20}} type="radio"></input>
       False <input style={{margin:20}} type="radio"></input>
@@ -29,7 +33,7 @@ export default function Question({ question,toggleQuestion }) {
       </label> */}
       {question.name}
       <input style={{margin:20, fontSize:15}} type="text"/>
-      <Question a={1}></Question>
+      <Question></Question>
       Delete question<input style={{margin:20}} type="checkbox" checked={question.complete} onChange={handleQuestionClick} />
     </div>
   )
