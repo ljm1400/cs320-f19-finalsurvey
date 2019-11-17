b72bc70c2fd6cd90678adc1d7afd23ece843c083
Louis <louismartin@umass.edu>
2019-11-17 15:27:18 -0500

    Created Front End for Login Page

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,5 +1,64 @@
 import React, { Component} from 'react';
 import { Redirect } from 'react-router-dom'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
 import '../css/style.css';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+    button: {
+        margin: theme.spacing(1),
+      },
+      input: {
+        display: 'Logged In',
+      },
+  }));
+
+export default class login extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <div className="header">
+                <h2>Log In</h2> 
+                <p></p>
+                <TextField
+                required
+                id="standard-required"
+                label="Enter Email"
+                Value="Email@Email.com"
+                margin="normal"
+                />
+                <p></p>
+                <TextField
+                required
+                id="standard-required"
+                label="Enter Password"
+                Value="password"
+                 margin="normal"
+                />
+                <p></p>
+                <Button variant="outlined">
+                Log In
+                </Button>
+                </div> 
+            </div> 
+        );
+      }
+    }
